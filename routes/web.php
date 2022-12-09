21d4da83fa84dd07f6c55deebecf2f0f02b81742
AsimAtıcı <asimatici51@gmail.com>
2022-12-09 23:48:35 +0300

    group link added

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Controller Kullanımı
 Route::get("/hakkimda", [UyelerController::class, "hakkimda"]);
+
+// Grup Link Kullanımı
+Route::group(["prefix" => "sayfalar"], function() {
+    Route::get('/hakkimda', function () {
+        return "hakkımda sayfası olacaktır";
+    });
+
+    Route::get('/iletisim', function () {
+        return "İletişim sayfası olacaktır";
+    });
+});
+
+
